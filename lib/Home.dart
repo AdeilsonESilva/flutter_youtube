5867c4d13d7907d99b3e22a0554e291cf2bbd6dc
Adeilson Silva <adeilson@gmail.com>
2020-07-11 13:07:21 -0300

    Configurado o cabecalho

---
+++ lib/Home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+        ),
+        title: Image.asset(
+          'assets/images/youtube.png',
+          width: 98,
+          height: 22,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
